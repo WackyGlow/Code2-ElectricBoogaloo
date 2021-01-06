55f45629b893657bbe14da3cb8bea8a1cd903d88
pete9991 <71692384+pete9991@users.noreply.github.com>
2021-01-06 10:12:17 +0100

    Update Movie.java

--- src/PMC/be/Movie.java
+++ src/PMC/be/Movie.java
@@ -1,4 +1,64 @@
 package PMC.be;
 
+import java.io.File;
+
 public class Movie {
+    private String name;
+    private float imdbRating;
+    private float rating;
+    private String filePath;
+    private String lastWatched;
+    private int id;
+
+public Movie(String name,float imdbRating,float rating,String filePath,String lastWatched,int id){
+    //File film = new File(filePath);
+}
+
+    public String getName() {
+        return name;
+    }
+
+    public float getImdbRating() {
+        return imdbRating;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public String getLastWatched() {
+        return lastWatched;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImdbRating(float imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void setLastWatched(String lastWatched) {
+        this.lastWatched = lastWatched;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
