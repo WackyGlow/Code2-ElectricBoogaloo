7e9726eac3ca84c9728bdd7421352a08a0f4e39a
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2021-01-12 12:39:50 +0100

    Create Junction class for DB

---
+++ src/PMC/be/GenreMovie.java
@@ -0,0 +1,14 @@
+package PMC.be;
+
+public class GenreMovie {
+
+    private int id;
+    private int movieId;
+    private int genreId;
+
+    public GenreMovie(int id, int movieId, int genreId) {
+        this.id = id;
+        this.movieId = movieId;
+        this.genreId = genreId;
+    }
+}
